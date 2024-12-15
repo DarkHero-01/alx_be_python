c1105541b2899c691a2fcd0cfd6012b8dde9d7a0
DarkHero <essabery99@gmail.com>
2024-12-15 20:07:02 +0100

    basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+resault = number1 + number2
+print (f"Addition {resault}")
+resault2 = number1 - number2
+print (f"Subtraction {resault2}")
+resault3 = number1 * number2
+print (f"Multiplication {resault3}")

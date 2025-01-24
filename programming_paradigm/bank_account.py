34f118ec78e26d62942461767cc9ae83ed0b60b5
DarkHero <essabery99@gmail.com>
2025-01-24 20:18:22 +0100

    commit

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,6 +1,6 @@
 class BankAccount:
-    def __init__(self, Current_Balance =0):
-        self.account_balance = Current_Balance
+    def __init__(self, initial_balance=0):
+        self.account_balance = initial_balance
 
     def deposit(self, amount):
         self.account_balance += amount
@@ -12,4 +12,4 @@ class BankAccount:
         return False
 
     def display_balance(self):
-        print(f"Current balance: ${self.account_balance:.2f}")
+        print(f"Current Balance: ${self.account_balance:.2f}")

4bea2378124d428f5e54d01020119ecdc031a8a4
DarkHero <essabery99@gmail.com>
2025-01-24 20:46:36 +0100

    commit

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,40 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = SimpleCalculator()
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(0, 0), 0)
+        self.assertEqual(self.calc.add(-5, -5), -10)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(10, 5), 5)
+        self.assertEqual(self.calc.subtract(5, 10), -5)
+        self.assertEqual(self.calc.subtract(0, 0), 0)
+        self.assertEqual(self.calc.subtract(-5, -5), 0)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(3, 4), 12)
+        self.assertEqual(self.calc.multiply(-3, 4), -12)
+        self.assertEqual(self.calc.multiply(0, 100), 0)
+        self.assertEqual(self.calc.multiply(-3, -4), 12)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(-10, 2), -5)
+        self.assertEqual(self.calc.divide(0, 5), 0)
+        self.assertIsNone(self.calc.divide(10, 0))
+        self.assertAlmostEqual(self.calc.divide(1, 3), 0.3333333, places=7)
+
+    def test_edge_cases(self):
+        self.assertEqual(self.calc.add(1e10, 1e10), 2e10)
+        self.assertEqual(self.calc.multiply(1e5, 1e5), 1e10)
+        self.assertAlmostEqual(self.calc.divide(1e-10, 1e-5), 1e-5, places=15)
+
+if __name__ == "__main__":
+    unittest.main()
